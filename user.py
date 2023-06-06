6e59d42fc3f5283ee2f24dc44694bfed698555e3
Marat <125448969+martini69lol@users.noreply.github.com>
2023-06-06 17:08:49 +0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,5 @@
+name = input('"What is your name? ')
+age = input("How old are you? ")
+live = input("Where are you live? ")
+
+print('This is ' + name + '\n' + 'It is ' + age + '\n' + '(S)he live in ' + live)
